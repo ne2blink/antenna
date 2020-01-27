02d9382ede0ba5f8908cfc487f4fb65d67ca1058
Need-Being <need-being@nbs.im>
2020-01-27 22:18:33 +0800

    update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,28 +4,24 @@ import (
 	"os"
 	"strings"
 
-	"go.uber.org/config"
+	"github.com/spf13/viper"
 )
 
 // New creates a new config
-func New(paths ...string) (config.Provider, error) {
-	if len(paths) == 0 {
-		if path, ok := os.LookupEnv("ANTENNA_CONFIG"); ok {
-			paths = append(paths, path)
-		} else {
-			paths = append(paths, "config.yml")
+func New(path string) (*viper.Viper, error) {
+	if c, ok := os.LookupEnv("ANTENNA_CONFIG"); ok {
+		path = c
+	}
+
+	v := viper.New()
+	if path != "" {
+		v.SetConfigFile(path)
+		if err := v.ReadInConfig(); err != nil {
+			return nil, err
 		}
 	}
-
-	opts := make([]config.YAMLOption, 0, len(paths)+1)
-	for _, path := range paths {
-		opts = append(opts, config.File(path))
-	}
-	replacer := strings.NewReplacer(".", "_")
-	opts = append(opts, config.Expand(func(key string) (string, bool) {
-		key = "ANTENNA_" + replacer.Replace(strings.ToUpper(key))
-		return os.LookupEnv(key)
-	}))
-
-	return config.NewYAML(opts...)
+	v.SetEnvPrefix("ANTENNA")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+	return v, nil
 }
