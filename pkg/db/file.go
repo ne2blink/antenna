650a01b75e8a8eee58d84a9e3e6872f4ee11f8ea
BB <i@sinon.me>
2020-01-29 12:22:34 +0900

    add bolt with file store

--- pkg/db/file.go
+++ pkg/db/file.go
@@ -1,6 +1,13 @@
 package db
 
-import "github.com/ne2blink/antenna/pkg/storage"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/ne2blink/antenna/pkg/storage"
+)
 
 type file struct {
 	path string
@@ -46,8 +53,23 @@ func (f *file) UnsubscribeAll(ChatID string) error {
 	return nil
 }
 
+func createChatsBucket(tx *bolt.Tx) error {
+	_, err := tx.CreateBucketIfNotExists([]byte("Chats"))
+	if err != nil {
+		return fmt.Errorf("create bucket: %s", err)
+	}
+	return nil
+}
+
+func createAppsBucket(tx *bolt.Tx) error {
+	_, err := tx.CreateBucketIfNotExists([]byte("Apps"))
+	if err != nil {
+		return fmt.Errorf("create bucket: %s", err)
+	}
+	return nil
+}
+
 func newFile(options map[string]interface{}) (storage.Store, error) {
-	// panic("todo")
 	path := "./file.db"
 	for k, v := range options {
 		switch k {
@@ -58,6 +80,23 @@ func newFile(options map[string]interface{}) (storage.Store, error) {
 		}
 	}
 	file := file{path: path}
+
+	db, err := bolt.Open(path, 0600, &bolt.Options{Timeout: 10 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	createChatsErr := db.Update(createChatsBucket)
+	if createChatsErr != nil {
+		return nil, createChatsErr
+	}
+
+	createAppsErr := db.Update(createAppsBucket)
+	if createAppsErr != nil {
+		return nil, createAppsErr
+	}
+
 	return &file, nil
 }
 
