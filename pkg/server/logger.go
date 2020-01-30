3dae5fd56900bd17a67d0735614df9d9b5dec124
Need-Being <need-being@nbs.im>
2020-01-30 17:58:03 +0800

    init web server

---
+++ pkg/server/logger.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func zapLogger(log *zap.SugaredLogger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start).Seconds()
+		status := c.Writer.Status()
+		log = log.With(
+			"remote", c.ClientIP(),
+			"host", c.Request.Host,
+			"method", c.Request.Method,
+			"uri", c.Request.URL.String(),
+			"duration", duration,
+			"status", status,
+		)
+		if err := c.Errors.String(); err != "" {
+			log = log.With("err", err)
+		}
+		switch {
+		case status >= 500 || status == 0:
+			log.Error()
+		case status >= 400:
+			log.Warn()
+		default:
+			log.Info()
+		}
+
+	}
+}
---
+++ pkg/server/server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ne2blink/antenna/pkg/antenna"
+	"github.com/ne2blink/antenna/pkg/storage"
+	"go.uber.org/zap"
+)
+
+// Server listens for the content.
+type Server struct {
+	engine  *gin.Engine
+	store   storage.AppStore
+	antenna *antenna.Antenna
+	log     *zap.SugaredLogger
+}
+
+// New creates a new server instance.
+func New(store storage.Store, antenna *antenna.Antenna, log *zap.SugaredLogger) *Server {
+	engine := gin.New()
+	engine.Use(zapLogger(log), gin.Recovery())
+	s := &Server{
+		engine:  engine,
+		store:   store,
+		antenna: antenna,
+		log:     log,
+	}
+	s.registerRoutes()
+	return s
+}
+
+// Listen starts the server.
+func (s *Server) Listen(addr string) error {
+	defer s.log.Sync()
+	s.log.Infof("starting at %s", addr)
+
+	web := &http.Server{
+		Addr:    addr,
+		Handler: s.engine,
+	}
+	if err := web.ListenAndServe(); err != http.ErrServerClosed {
+		s.log.Errorf("listen: %s", err.Error())
+		return err
+	}
+
+	return nil
+}
